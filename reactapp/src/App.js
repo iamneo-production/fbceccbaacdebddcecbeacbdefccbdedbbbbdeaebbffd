fc10b6cb2fecfb7342b30a0dbc79803033cfe463
Iamneo <techadmin@iamneo.ai>
2023-08-06 15:41:27 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { getUserIpDetails, getTopThree } from './functions/destructuring';
 
 function App() {
+  const userDetails = getUserIpDetails({
+    userName: 'test',
+    userIp: '127.0.0.1',
+  });
+  console.log(userDetails);
+
+  const studentMarks = [90, 85];
+  const topThree = getTopThree(studentMarks);
+  console.log(topThree);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Username: {userDetails[0]}</p>
+      <p>User IP: {userDetails[1]}</p>
+      <ul>
+        {topThree.map((mark, index) => (
+          <li key={index}>{mark}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ reactapp/src/functions/destructuring.js
@@ -0,0 +1,19 @@
+function getUserIpDetails(userResponse = {}) {
+    const { userName, userIp = '0.0.0.0' } = userResponse;
+    return [userName, userIp];
+  }
+  
+  // Function to get top three student details
+  function getTopThree(marks) {
+    if (marks.length >= 3) {
+      return [marks[0], marks[1], marks[2]];
+    } else {
+      let paddedMarks = [...marks];
+      while (paddedMarks.length < 3) {
+        paddedMarks.push(null);
+      }
+      return paddedMarks;
+    }
+  }
+  
+  export { getUserIpDetails, getTopThree };
